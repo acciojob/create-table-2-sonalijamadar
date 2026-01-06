1ffebbb794f0bb14607929b96513a5ae1ac40497
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2026-01-06 12:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createTable() {
     //Write your code here
-	const table = document.getElementById(mytable);
+	const table = document.getElementById(myTable);
 	table.innerHTML = "";
 
 	const rn = prompt("Input number of rows");
@@ -8,18 +8,18 @@ function createTable() {
 	const rows = Number(rn);
 	const cols = Number(cn);
 
-	if(isNaN(rows) || isNan(cols)){
+	if (isNaN(rows) || isNan(cols)) {
 		return;
 	}
 	if(rows <= 0 || cols <= 0){
-		alert("Please Enter Positive Number");
+		alert("Please enter positive numbers greater than 0");
 		return;
 	}
 	for(let i = 0; i < rows; i++){
 		const tr = document.createElement("tr");
 		for(let j = 0; j < cols; j++){
 			const td = document.createElement("td");
-			td.textContent = `Rows-${i} Column-${j}`;
+			td.textContent = `Row-${i} Column-${j}`;
 			tr.appendChild(td);
 		}
 		table.appendChild(tr);
